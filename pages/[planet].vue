a8d54b232c3b1758ca2ffb8cfada754fc1a78bf3
patricklaireiter <95612263+patricklaireiter@users.noreply.github.com>
2023-05-02 11:13:27 +0200

    buttons

--- pages/[planet].vue
+++ pages/[planet].vue
@@ -1,6 +1,8 @@
 <script setup>
 const route = useRoute();
 
+const currentCard = ref("overview");
+
 const planets = ref(null);
 const planet = computed(function () {
   if (!planets.value) {
@@ -21,7 +23,6 @@ onMounted(async () => {
 <template>
   <v-container>
     <h1>{{ route.params.planet }}</h1>
-
     <p>{{ planet.name }}</p>
 
     <v-row>
@@ -39,12 +40,26 @@ onMounted(async () => {
             <h2>{{ planet.name }}</h2>
           </v-card-title>
           <v-card-text>
-            <p>{{ planet.description }}</p>
-            <p>{{ planet.revolution }}</p>
-            <p>{{ planet.rotation }}</p>
-            <p>{{ planet.radius }}</p>
-            <p>{{ planet.temperature }}</p>
-
+            <div v-if="currentCard === 'overview'">
+              <p>{{ planet.description }}</p>
+              <p>{{ planet.revolution }}</p>
+              <p>{{ planet.rotation }}</p>
+              <p>{{ planet.radius }}</p>
+              <p>{{ planet.temperature }}</p>
+            </div>
+            <div v-if="currentCard === 'structure'">
+              <p>{{ planet.structure }}</p>
+            </div>
+            <div v-if="currentCard === 'geology'">
+              <p>{{ planet.geology }}</p>
+            </div>
+            <v-row>
+              <v-card>
+                <v-btn @click="currentCard = 'overview'" variant="outlined"> Overview </v-btn>
+                <v-btn @click="currentCard = 'structure'" variant="outlined"> Structure </v-btn> 
+                <v-btn @click="currentCard = 'geology'" variant="outlined"> Geology</v-btn>
+              </v-card>
+            </v-row>
             <div>
               <button type="submit">siu</button>
             </div>
