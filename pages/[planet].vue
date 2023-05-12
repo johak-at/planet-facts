fa2800435256ccc9d09401ab4562e9ba234a1f88
patricklaireiter <95612263+patricklaireiter@users.noreply.github.com>
2023-05-12 10:11:36 +0200

    button

--- pages/[planet].vue
+++ pages/[planet].vue
@@ -1,28 +1,6 @@
-<script setup>
-const route = useRoute();
-
-const currentCard = ref("overview");
-
-const planets = ref(null);
-const planet = computed(function () {
-  if (!planets.value) {
-    return "loading";
-  }
-
-  return planets.value.planets.filter(
-    (p) => p.name.toLowerCase() === route.params.planet
-  )[0];
-});
-
-onMounted(async () => {
-  const res = await fetch("/data.json");
-  planets.value = await res.json();
-});
-</script>
-
 <template>
   <v-container>
-    <h1>{{ route.params.planet }}</h1>
+    <h1>{{ planet.name }}</h1>
     <p>{{ planet.name }}</p>
 
     <v-row>
@@ -41,36 +19,70 @@ onMounted(async () => {
           </v-card-title>
           <v-card-text>
             <div v-if="currentCard === 'overview'">
+              <h3>Overview</h3>
               <p>{{ planet.description }}</p>
-              <p>{{ planet.revolution }}</p>
-              <p>{{ planet.rotation }}</p>
-              <p>{{ planet.radius }}</p>
-              <p>{{ planet.temperature }}</p>
+              <p>Revolution: {{ planet.revolution }}</p>
+              <p>Rotation: {{ planet.rotation }}</p>
+              <p>Radius: {{ planet.radius }}</p>
+              <p>Temperature: {{ planet.temperature }}</p>
             </div>
             <div v-if="currentCard === 'structure'">
+              <h3>Structure</h3>
               <p>{{ planet.structure }}</p>
             </div>
             <div v-if="currentCard === 'geology'">
+              <h3>Geology</h3>
               <p>{{ planet.geology }}</p>
             </div>
             <v-row>
-              <v-card>
-                <v-btn @click="currentCard = 'overview'" variant="outlined"> Overview </v-btn>
-                <v-btn @click="currentCard = 'structure'" variant="outlined"> Structure </v-btn> 
-                <v-btn @click="currentCard = 'geology'" variant="outlined"> Geology</v-btn>
-              </v-card>
+              <v-col cols="12">
+                <v-btn @click="currentCard = 'overview'" variant="outlined">Overview</v-btn>
+              </v-col>
+              <v-col cols="12">
+                <v-btn @click="currentCard = 'structure'" variant="outlined">Structure</v-btn>
+              </v-col>
+              <v-col cols="12">
+                <v-btn @click="currentCard = 'geology'" variant="outlined">Geology</v-btn>
+              </v-col>
             </v-row>
-            <div>
-              <button type="submit">siu</button>
-            </div>
-            <div>
-              <button type="submit">
-                <router-link :to="{ name: 'index' }">Back</router-link>
-              </button>
-            </div>
+            <v-row>
+              <v-col cols="6">
+                <button type="submit">siu</button>
+              </v-col>
+              <v-col cols="6">
+                <button type="submit">
+                  <router-link :to="{ name: 'index' }">Back</router-link>
+                </button>
+              </v-col>
+            </v-row>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const currentCard = ref('overview');
+
+const planets = ref(null);
+const planet = computed(() => {
+  if (!planets.value) {
+    return 'loading';
+  }
+
+  return planets.value.planets.filter(
+    (p) => p.name.toLowerCase() === route.params.planet
+  )[0];
+});
+
+onMounted(async () => {
+  const res = await fetch('/data.json');
+  planets.value = await res.json();
+});
+</script>
